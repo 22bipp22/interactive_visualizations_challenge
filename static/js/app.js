64f9c2dfcc0f7fde29dd58c50705204b417df58e
22bipp22 <tammibippert@gmail.com>
2020-10-17 18:40:49 -0500

    clean up the code to remove console.logs

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 //Read the json, assign it to a variable then call the function to populate the dropdown menu
 d3.json("samples.json").then((data) => {
-    let bellyButtonData = data;
     mydropDown(data);
     
 });  
@@ -8,16 +7,16 @@ d3.json("samples.json").then((data) => {
 //Populate the dropdown with the data from json
 function mydropDown(data) {
     
-    //Grab the OTU number from the data (names field)
+    //Grab the test subject ID # from the data (names field)
     let nameVal = data.names;
 
     let dropDown = d3.select("#selDataset");
     dropDown.html("");
 
-    //Populate the dropdown options with the OTU number, adding the letters OTU before each one
+    //Populate the dropdown options with the sample "names" (test subject ID #)
     nameVal.forEach((name) => {
         
-        let selectionVal = "OTU " + name;
+        let selectionVal = name;
             
         let options = dropDown.append("option");
         options.text(selectionVal);
@@ -25,7 +24,6 @@ function mydropDown(data) {
     
     });
 };
-
 
 // Dropdown selection event handler
 function optionChanged() {
@@ -63,35 +61,43 @@ function buildPlot(key, microbe) {
         let otuSamples = sampleData.samples;
         // console.log(otuSamples);
         let sampleId = otuSamples[key];
-        
 
-        // console.log(sampleId);
-        let otuIds = sampleId.otu_ids.slice(0,10);
-        // console.log(otuIds);
+        // Slice the data to only show the top 10 Microbes
+        let otuIds = sampleId.otu_ids.slice(0,10);        
         let otuValues = sampleId.sample_values.slice(0,10);
-        // console.log(otuValues);
+        let otuLabels = sampleId.otu_labels.slice(0,10);
+        
+        
+        //Reverse the order of the data to display most prevelant at the top of the bar chart
+        let otuIdsR = otuIds.reverse();
+        let otuValuesR = otuValues.reverse();
+        let otuLabelsR = otuLabels.reverse();
         
         let otuIdsLabels = [];
-        otuIds.forEach((id) => {
+        otuIdsR.forEach((id) => {
             // let text = "OTU "
             let otuIdsLabel = "OTU " + (id);
             // console.log(otuIdsLabel);
             otuIdsLabels.push(otuIdsLabel);
         });
+
+        
         // console.log(otuIdsLabels);
 
         let trace1 = {
-            x: otuValues,
+            x: otuValuesR,
             y: otuIdsLabels,
             type: "bar",
-            orientation: 'h'
+            orientation: 'h',
+            text: otuLabelsR
 
         }
 
         let dataTrace = [trace1];
 
         let layout = {
-            title: `Data for OTU ${microbe}`
+            title: `Microbes for Test Subject ID #${microbe}`,
+            hovermode: "closest"
         }
 
         Plotly.newPlot("plot", dataTrace, layout);
