0b2d8c66c1d249a9b1ce8a4053042614a5c82aa2
22bipp22 <tammibippert@gmail.com>
2020-10-17 23:56:09 -0500

    added bubble graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 //Read the json, assign it to a variable then call the function to populate the dropdown menu
 d3.json("samples.json").then((data) => {
     mydropDown(data);
-    
+    // console.log(data)
 });  
 
 //Populate the dropdown with the data from json
@@ -37,7 +37,8 @@ function optionChanged() {
         // console.log(bellyButtonData);
         let newIndex = getKeyByValue(bellyButtonData, newValue)
         console.log(newIndex, newValue);
-        buildPlot(newIndex, newValue);
+        buildPlots(newIndex, newValue);
+        populateMetaData(newIndex, newValue);
         }) 
     
     //Function to get the key from the data
@@ -52,10 +53,11 @@ function optionChanged() {
 let microbe = 940;
 let key = 0;    
 
-buildPlot(key, microbe);
+buildPlots(key, microbe);
+populateMetaData(key, microbe);
 
 //Builds the horizontal bar chart on the webpage
-function buildPlot(key, microbe) {
+function buildPlots(key, microbe) {
     d3.json("samples.json").then((sampleData) => {
         // console.log(sampleData);
         let otuSamples = sampleData.samples;
@@ -67,23 +69,20 @@ function buildPlot(key, microbe) {
         let otuValues = sampleId.sample_values.slice(0,10);
         let otuLabels = sampleId.otu_labels.slice(0,10);
         
-        
         //Reverse the order of the data to display most prevelant at the top of the bar chart
         let otuIdsR = otuIds.reverse();
         let otuValuesR = otuValues.reverse();
         let otuLabelsR = otuLabels.reverse();
         
+        //create labels for display on the y axis of horizontal bar
         let otuIdsLabels = [];
-        otuIdsR.forEach((id) => {
-            // let text = "OTU "
+        otuIds.forEach((id) => {
             let otuIdsLabel = "OTU " + (id);
-            // console.log(otuIdsLabel);
             otuIdsLabels.push(otuIdsLabel);
         });
-
-        
-        // console.log(otuIdsLabels);
-
+        otuIdsLabelsR = otuIdsLabels.reverse();
+      
+        //Create the Horizontal Bar chart
         let trace1 = {
             x: otuValuesR,
             y: otuIdsLabels,
@@ -100,12 +99,77 @@ function buildPlot(key, microbe) {
             hovermode: "closest"
         }
 
-        Plotly.newPlot("plot", dataTrace, layout);
-    
+        Plotly.newPlot("bar", dataTrace, layout);
+
+        //Create the Bubble Chart using all data for given subject
+        let trace2 = {
+            x: sampleId.otu_ids,
+            y: sampleId.sample_values,
+            mode: "markers",
+            marker: {
+                size: sampleId.sample_values,
+                color: sampleId.otu_ids
+            },
+            text: sampleId.otu_labels
+
+        }
+
+        let dataTrace2 = [trace2];
+
+        let layout2 = {
+            title: `Microbes for Test Subject ID #${microbe}`
+            
+        }
+        Plotly.newPlot("bubble", dataTrace2, layout2);
+
     });
 };
 
+function populateMetaData(key, value) {
+    d3.json("samples.json").then((data) => {
+        
+        let metaData = data.metadata;
+        console.log(metaData);
+        let metaSelected = metaData[key];
+        console.log(metaSelected);
 
+        let metaId = value;
+        let metaEthnic = metaSelected.ethnicity;
+        let metaGender = metaSelected.gender;
+        let metaAge = metaSelected.age;
+        let metaLocation = metaSelected.location;
+        let metaBbtype = metaSelected.bbtype;
+        let metaWfreq = metaSelected.wfreq;
+
+        //Add the demographic data to the Demographic Info Box
+        let info = d3.select("#sample-metadata");
+
+        info.html("");
+
+        info.append('h6').text(`Id: ${metaId}`);
+        info.append('h6').text(`Ethnicity: ${metaEthnic}`);
+        info.append('h6').text(`Gender: ${metaGender}`);
+        info.append('h6').text(`Age: ${metaAge}`);
+        info.append('h6').text(`Location: ${metaLocation}`);
+        info.append('h6').text(`bbtype: ${metaBbtype}`);
+        info.append('h6').text(`wfreq: ${metaWfreq}`);
+
+        //Use the metaData Washing Frequency to populate the gauge chart
+        let trace3 = [
+            {
+                domain: { x: [0, 1], y: metaWfreq },
+                value: 270,
+                title: { text: "Speed" },
+                type: "indicator",
+                mode: "gauge+number"
+            }
+        ];
+                
+        let layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+        
+        Plotly.newPlot('gauge', data, layout);
+    });
+}
 
 // function unpack(rows, index) {
 //     return rows.map(function(row) {
